30a4ded665283c81f60b4771116b17cbdc32a8c3
renmay <renmay@qq.com>
2018-09-25 17:09:07 +0800

    second

---
+++ palindromic number.js
@@ -0,0 +1,20 @@
+// var isPalindrome = function(x) {
+//     var x = x.toString()
+//     x = x.charAt(x.length – 1)
+//     console.log(x)
+//     // if (x > 0 && ) {
+
+//     // }
+// };
+
+// isPalindrome(123)
+
+
+var str = "1,2,3,4,5,";
+  
+  var str1 = str.substring(str.length - 2);
+  // var str2 = str.substr(0, str.length - 1);
+  // var str3 = str.slice(0, str.length - 1);
+  
+  // console.log(str,str1,str2,str3);
+  console.log(str,str1);
