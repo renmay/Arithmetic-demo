63c6c82c48132a9b75c365ee2eeecc46f9c082b4
renmay <renmay@qq.com>
2018-10-10 10:48:39 +0800

    maxPrimeFactor

--- projectEulor/js/findPrimeFactor.js
+++ projectEulor/js/findPrimeFactor.js
@@ -1,3 +1,15 @@
-function getPrime(){
-	
+function findMaxPrimeFactor(n) {
+    var m = 1;
+    for (var i = 2; n > 1; ++i) {
+        if (n % i == 0) {
+            n = n/i;
+            m = i;
+            while (n % i == 0) {
+                n = n/i;
+            }
+        }
+    }
+    return m;
 }
+
+console.log(findMaxPrimeFactor(600851475143))
