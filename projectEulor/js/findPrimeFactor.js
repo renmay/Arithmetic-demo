fd476e3a6ca1d6ca77e03cd141a52101b934060d
renmay <renmay@qq.com>
2018-10-10 11:50:34 +0800

    ewd

--- projectEulor/js/findPrimeFactor.js
+++ projectEulor/js/findPrimeFactor.js
@@ -1,15 +1,22 @@
+// Largest prime factor
+// Problem 3
+// The prime factors of 13195 are 5, 7, 13 and 29.
+//
+// What is the largest prime factor of the number 600851475143 ?
+
 function findMaxPrimeFactor(n) {
     var m = 1;
-    for (var i = 2; n > 1; ++i) {
+    for (var i = 2; i <= n; i++) {
         if (n % i == 0) {
             n = n/i;
             m = i;
             while (n % i == 0) {
                 n = n/i;
+                console.log('刚好')
             }
         }
     }
     return m;
 }
 
-console.log(findMaxPrimeFactor(600851475143))
+console.log(findMaxPrimeFactor(20))
---
+++ projectEulor/py/findMaxPrimeFactor.py
@@ -0,0 +1,21 @@
+# Largest prime factor
+# Problem 3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+
+def findMaxPrimeFactor(n):
+    m = 1
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            n = n / i
+            m = i
+            while n % i == 0:
+                n = n / i
+        i = i+1
+    return m
+
+
+result = findMaxPrimeFactor(20)
+print(result)
