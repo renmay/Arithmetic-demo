e4df61ae857153312c716ed08c03ea34929e51cc
renmay <renmay@qq.com>
2018-10-10 17:55:06 +0800

    6

---
+++ projectEulor/js/6-SumSquareDifference.js
@@ -0,0 +1,23 @@
+// Sum square difference
+// Problem 6
+// The sum of the squares of the first ten natural numbers is,
+//
+// 12 + 22 + ... + 102 = 385
+// The square of the sum of the first ten natural numbers is,
+//
+// (1 + 2 + ... + 10)2 = 552 = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+//
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+// 前10个自然数的平方和是，
+// 12 + 22 +…+ 102 = 385
+// 前10个自然数之和的平方是，
+// (1 + 2 +…+ 10)2 = 552 = 3025
+// 因此之间的区别的平方和的第十个自然数的平方之和是3025−385 = 2640。
+// 求前100个自然数的平方和与和的平方的差值。
+
+
+function getSum(n) {
+
+}
